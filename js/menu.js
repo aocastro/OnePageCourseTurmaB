fd13b6a28a58d80783608c45c686adb4a3c46bd8
Adriano Oliveira Castro <56028189+aocastro@users.noreply.github.com>
2021-09-13 15:24:51 +0000

    Commit da aula do dia 13/09/2021

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
         e.preventDefault()
         $('#menu').toggle('slow')
     })
+
+    // Criar uma função para monitorar os cliques nos links
+    $('a').click(function(e){
+        e.preventDefault()
+        // Capturar qual é a página que desejo abrir
+        let url = $(this).attr('href')
+        $('#'+url).show()
+    })
 })
